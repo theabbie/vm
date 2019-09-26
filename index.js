adb7d1777d2b4f9c58dfcc99bb59875ec0da90eb
abhishek <abhishek7gg7@gmail.com>
2019-09-26 22:57:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require('express');
-
-const app = express();
-
-app.get("/home", (req,res) => {
-res.sendFile(__dirname+'/index.html');
-});
-
-app.get('/*', (req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/event-stream',
-        'Cache-Control': 'no-cache',
-        'Connection': 'keep-alive',
-    });
-    setInterval(function() {res.write("hello world")},2000);
-});
-
-app.listen(process.env.PORT);
+const http = require('http');
+http.createServer((request, response) => {
+  response.writeHead(200, {
+    Connection: 'keep-alive',
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache'
+  });
+  let id = 1;
+  // Send event every 3 seconds or so forever...
+  setInterval(() => {
+    response.write(
+      `event: myEvent\nid: ${id}\ndata:This is event ${id}.`
+    );
+    response.write('\n\n');
+    id++;
+  }, 3000);
+}).listen(process.env.PORT);

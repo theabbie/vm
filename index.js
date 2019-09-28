299dce2edcccf47e0a839c1429380c179f891582
abhishek <abhishek7gg7@gmail.com>
2019-09-28 23:14:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var vm = require("vm");
 
 app.get("/*", function(req,res) {
 axios("https://thedb.now.sh/app.js").then( function(x) {
-vm.runInNewContext(x.data,{axios: axios, req: req, res: res});
+vm.runInNewContext(x.data,{axios: axios, req: req, res: res, require: require});
 })
 })
 
